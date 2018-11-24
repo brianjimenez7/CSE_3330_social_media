ad8a281faf5a0d3436d0c03ea01f00716dda070f
ezcire89 <ezcire@me.com>
2018-11-23 22:37:19 -0600

    added pictures

--- home.php
+++ home.php
@@ -8,6 +8,9 @@
         div.a {
             text-indent: 45%;
                 }   
+                div.b {
+            text-indent: 20%;
+                }   
     </style>
 
 
@@ -26,7 +29,38 @@
             }
         </script>
 
-            <div class = "a" >
+            <div class = "b" >
+        <?php 
+
+                            
+
+        session_start();
+        include_once 'dbh.php';
+
+        $user=$_SESSION['userName'];
+        
+        $mysql_query = "SELECT Profile_ID FROM Profile where Username='$user'";
+        $result = mysqli_query($conn, $mysql_query);
+        $id=mysqli_fetch_array($result);
+        // echo "id: " . $id[0] . "<br>";
+
+        $mysql_query = "SELECT Profile_Images FROM Profile where Username='$user'";
+        $result = mysqli_query($conn, $mysql_query);
+        $Pro_image=mysqli_fetch_array($result);
+
+
+        if( $Pro_image[0] == null )
+        {
+            echo "No Profile pick. Go to settings to upload!";
+        }
+        else
+        {
+
+            echo '<div class="c"> <img src="'.$Pro_image[0].'" width="300" height="250" /></div>';
+        }
+
+
+        ?>
         <p>
             <a href="createPage.php">Create a page</a>
         </p>
@@ -36,11 +70,8 @@
         <p>
             <a href="ViewAPage.php">View a page</a>
         </p>
-        
         <p>
-            <a href="createPost.php">Post a post </a>
 
-        <p>
             <a href="createPost.php">Create Post!</a>
         </p>
 
